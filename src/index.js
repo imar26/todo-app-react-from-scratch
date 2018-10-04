4e89e08c1e598f8cf48a92a0af1de56b4e79d7a8
imar26 <aadeshranderia26@gmail.com>
2018-10-03 21:45:42 -0400

    todo add function implemented

--- src/index.js
+++ src/index.js
@@ -4,9 +4,33 @@ import ReactDOM from "react-dom";
 // Import Styles
 import './style.css';
 
+const classNames = {
+  TODO_ITEM: 'todo-container',
+  TODO_CHECKBOX: 'todo-checkbox',
+  TODO_TEXT: 'todo-text',
+  TODO_DELETE: 'todo-delete',
+}
+
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      todoList: []
+    }
+  } 
+
   newTodo() {
-    alert('New TODO button clicked!');
+    var newTodo = {
+      id: Math.random(),
+      title: 'This is my new todo item'
+    }
+    
+    this.state.todoList.push(newTodo);
+
+    this.setState({
+      todoList: this.state.todoList
+    });
   }
 
   render() {
@@ -16,25 +40,20 @@ class App extends Component {
         <span>Item count: <span id="item-count">0</span></span>
         <span>Unchecked count: <span id="unchecked-count">0</span></span>
       </div>
+      <input type="text" ref="todoItem" placeholder="Enter Todo Item" required />
       <button className="button center" onClick={() => this.newTodo()}>New TODO</button>
-      <ul id="todo-list" className="todo-list"></ul>
+      <ul id="todo-list" className="todo-list">
+        {
+          this.state.todoList.map(todo => {
+            return <li className={classNames.TODO_ITEM} key={todo.id}><p className={classNames.TODO_TEXT}>{todo.title}</p></li>
+          })
+        }
+      </ul>
     </div>
   }
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-// const classNames = {
-//   TODO_ITEM: 'todo-container',
-//   TODO_CHECKBOX: 'todo-checkbox',
-//   TODO_TEXT: 'todo-text',
-//   TODO_DELETE: 'todo-delete',
-// }
-
-// const list = document.getElementById('todo-list')
 // const itemCountSpan = document.getElementById('item-count')
 // const uncheckedCountSpan = document.getElementById('unchecked-count')
-
-// function newTodo() {
-//   alert('New TODO button clicked!')
-// }
